94d4fd48da1334762655522b8c28cc10c71e17a0
USUARIOUPT <USUARIOUPT>
2017-03-18 11:49:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/usuarioupt/fragmento/Contenido.java
@@ -0,0 +1,17 @@
+package com.example.usuarioupt.fragmento;
+
+/**
+ * Created by USUARIOUPT on 18/03/2017.
+ */
+
+public class Contenido {
+    static String[] Titulos ={
+            "Primer Parrafo",
+            "Segundo Parrafo"
+    };
+
+    static String[] Parrafos ={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla feugiat ante quis augue vestibulum, non accumsan ligula efficitur. Phasellus blandit luctus neque, sit amet fermentum ex. Proin vitae nunc id massa vehicula hendrerit vel a diam.",
+            "Phasellus posuere dui vitae quam bibendum vulputate bibendum egestas elit. Pellentesque sed efficitur tellus, et tempor libero."
+    };
+}
